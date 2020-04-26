cb3dfed368c7f0dc3f7e1a02979a5c6b59f08170
vrompasa <ville.rompasaari@gmail.com>
2020-04-26 23:43:10 +0300

    Update saving, add toggle for isolated nodes

--- src/authnet.py
+++ src/authnet.py
@@ -16,7 +16,7 @@ def generate_example_dict():
 
     return dict
 
-def generate_graph(author_dict):
+def generate_graph(author_dict, all_nodes):
     coauthor_graph = nx.Graph()
 
     coauthor_graph.add_nodes_from(author_dict.keys())
@@ -26,7 +26,7 @@ def generate_graph(author_dict):
             if coauthor[2] >= 2 and not coauthor_graph.has_edge(author, coauthor[0]):
                 coauthor_graph.add_edge(author, coauthor[0])
             else:
-                if not coauthor_graph.has_node(coauthor[0]):
+                if not coauthor_graph.has_node(coauthor[0]) and all_nodes:
                     coauthor_graph.add_node(coauthor[0])
 
     return coauthor_graph
@@ -35,11 +35,11 @@ def show_graph(graph):
     nx.draw_networkx(graph, node_size=30, alpha=0.75, with_labels=False)
     plt.show()
 
-def save_graph(graph):
-    plt.figure(num=None, figsize=(20, 20), dpi=80)
+def save_graph(graph, graph_name):
+    plt.figure(num=None, figsize=(20, 20), dpi=100)
     plt.axis('off')
     nx.draw_networkx(graph, node_size=5, alpha=0.75, with_labels=False)
-    plt.savefig("graph.pdf", bbox_inches="tight")
+    plt.savefig("{}.pdf".format(graph_name[13:]), bbox_inches="tight")
 
 def search_communities(graph, graph_name):
     community_gen = community.k_clique_communities(graph, 2)
@@ -52,6 +52,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", help=".pickle file containing the dictionary.")
     parser.add_argument("-p", action="store_true", default=False, help="If set, plots the graph.")
+    parser.add_argument("-a", action="store_true", default=False, help="If set, includes isolated nodes to graph.")
     parser.add_argument("-s", action="store_true", default=False, help="If set, saves the graph.")
     parser.add_argument("-c", action="store_true", default=False, help="If set, analyses communities with k-clique k=2.")
     args = parser.parse_args()
@@ -63,11 +64,11 @@ if __name__ == "__main__":
         author_dict = generate_example_dict()
 
     if author_dict:
-        coauthor_graph = generate_graph(author_dict)
+        coauthor_graph = generate_graph(author_dict, args.a)
         if args.c:
             search_communities(coauthor_graph, args.d if args.d else "example.pickle")
         if args.p:
             show_graph(coauthor_graph)
         if args.s:
-            save_graph(coauthor_graph)
+            save_graph(coauthor_graph, args.d)
     
