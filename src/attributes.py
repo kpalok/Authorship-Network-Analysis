34569079251743982f3d3f166d748e3a1942f62b
jhalkola <joona.halkola@hotmail.com>
2020-04-26 10:23:02 +0300

    Global attributes calcs

---
+++ src/attributes.py
@@ -0,0 +1,66 @@
+import networkx as nx
+import statistics as stat
+import matplotlib.pyplot as plt
+import pandas as pd
+import authnet
+import argparse
+import queries
+
+def summarize_graph(graph):
+    calculations = []
+
+    # average clustering coefficient
+    coefficient_dict = nx.clustering(graph)
+    coefficient = stat.mean(coefficient_dict.values())
+    calculations.append(["Avg clustering coefficient", coefficient])
+
+    # average and variance of degree centrality
+    degree_centrality = nx.degree_centrality(graph)
+    avg_degree_centrality = stat.mean(degree_centrality.values())
+    var_degree_centrality = stat.pvariance(degree_centrality.values())
+    calculations.append(["Avg degree centrality", avg_degree_centrality])
+    calculations.append(["Var degree centrality", var_degree_centrality])
+
+    # average and variance of closeness centrality
+    closeness_centrality = nx.closeness_centrality(graph)
+    avg_closeness_centrality = stat.mean(closeness_centrality.values())
+    var_closeness_centrality = stat.pvariance(closeness_centrality.values())
+    calculations.append(["Avg closeness centrality", avg_closeness_centrality])
+    calculations.append(["Var closeness centrality", var_closeness_centrality])
+
+    # average and variance of betweeness centrality
+    betweeness_centrality = nx.betweenness_centrality(graph)
+    avg_betweeness_centrality = stat.mean(betweeness_centrality.values())
+    var_betweeness_centrality = stat.pvariance(betweeness_centrality.values())
+    calculations.append(["Avg betweeness centrality", avg_betweeness_centrality])
+    calculations.append(["Var betweeness centrality", var_betweeness_centrality])
+
+    # diameter of the graph
+    diameter_list = []
+    for i in nx.connected_components(graph):
+        sub = graph.subgraph(i)
+        diameter_list.append(nx.diameter(sub))
+    diameter = max(diameter_list)
+
+    calculations.append(["Graph diameter", diameter])
+    
+    # giant component of the graph
+    giant_component_size = len(max(nx.connected_components(graph), key = len))
+    calculations.append(["Giant component size", giant_component_size])
+
+    # data visualised in table
+    print("******* Data table *******\n")
+    df = pd.DataFrame(calculations, columns=["Variable", "Value"])
+    print(df.round(3))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", help=".pickle file containing the dictionary.")
+    args = parser.parse_args()
+
+    if args.d:
+        author_dict = queries.loadPickleDict(args.d)
+
+    if author_dict:
+        coauthor_graph = authnet.generate_graph(author_dict)
+        summarize_graph(coauthor_graph)
