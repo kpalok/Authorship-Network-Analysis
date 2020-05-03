4270087977bef7dc83c7bf3817345f06f915068d
vrompasa <ville.rompasaari@gmail.com>
2020-05-03 23:38:10 +0300

    Add power law fitting for countries

--- src/power_law.R
+++ src/power_law.R
@@ -33,6 +33,8 @@ leung <- getData("../data/dc_auth_leung.csv")
 yu <- getData("../data/dc_auth_yu.csv")
 leung.aff <- getData("../data/dc_aff_leung.csv")
 yu.aff <- getData("../data/dc_aff_yu.csv")
+leung.country <- getData("../data/dc_aff_leung_country.csv")
+yu.country <- getData("../data/dc_aff_yu_country.csv")
 
 
 # Plot fit
@@ -47,8 +49,13 @@ plotFit(leung.aff$dist, "Leung")
 mtext(text=expression(bold("Power law fit for all affiliations when x"[min]*" is estimated")),
       outer=TRUE, cex=1)
 
+plotFit(yu.country$dist, "Yu")
+plotFit(leung.country$dist, "Leung")
+mtext(text=expression(bold("Power law fit for affiliations grouped by country")),
+      outer=TRUE, cex=1)
 
-# Set estimates of xmin to 120
+
+# Set estimates of xmin to 120 (no need to do this for countries)
 yu$dist <- estimateParameters(yu$dist, 120)
 leung$dist <- estimateParameters(leung$dist, 120)
 yu.aff$dist <- estimateParameters(yu.aff$dist, 120)
@@ -68,24 +75,32 @@ mtext(text=expression(bold("Power law fit for all affiliations when x"[min]*" is
 
 
 # Plot all values in a log-log plot
-par(mfrow=c(2,2))
+par(mfrow=c(2,3))
 plot(sort(yu$data, decreasing=T), log="xy", ylab="degree",
      main="Yu", pch=20, cex=0.5, type="p")
-plot(sort(leung$data, decreasing=T), log="xy", ylab="degree",
-     main="Leung", pch=20, cex=0.5, type="p")
 plot(sort(yu.aff$data, decreasing=T), log="xy", ylab="degree",
      main="Yu Affiliations", pch=20, cex=0.5, type="p")
+plot(sort(yu.country$data, decreasing=T), log="xy", ylab="degree",
+     main="Yu Country", pch=20, cex=0.5, type="p")
+plot(sort(leung$data, decreasing=T), log="xy", ylab="degree",
+     main="Leung", pch=20, cex=0.5, type="p")
 plot(sort(leung.aff$data, decreasing=T), log="xy", ylab="degree",
      main="Leung Affiliations", pch=20, cex=0.5, type="p")
+plot(sort(leung.country$data, decreasing=T), log="xy", ylab="degree",
+     main="Leung Country", pch=20, cex=0.5, type="p")
 
 
 # Calculate P-values
-leung$bsp <- bootstrap_p(leung$dist, no_of_sims=1000, threads=2)
 yu$bsp <- bootstrap_p(yu$dist, no_of_sims=1000, threads=2)
-leung.aff$bsp <- bootstrap_p(leung.aff$dist, no_of_sims=1000, threads=2)
 yu.aff$bsp <- bootstrap_p(yu.aff$dist, no_of_sims=1000, threads=2)
+yu.country$bsp <- bootstrap_p(yu.country$dist, no_of_sims=1000, threads=2)
+leung$bsp <- bootstrap_p(leung$dist, no_of_sims=1000, threads=2)
+leung.aff$bsp <- bootstrap_p(leung.aff$dist, no_of_sims=1000, threads=2)
+leung.country$bsp <- bootstrap_p(leung.country$dist, no_of_sims=1000, threads=2)
 
-pvalues <- matrix(c(yu$bsp$p, yu.aff$bsp$p, leung$bsp$p, leung.aff$bsp$p), nrow=1, ncol=4)
-colnames(pvalues) <- c("Yu", "Yu Aff", "Leung", "Leung Aff")
+pvalues <- matrix(c(yu$bsp$p, yu.aff$bsp$p, yu.country$bsp$p,
+                    leung$bsp$p, leung.aff$bsp$p, leung.country$bsp$p),
+                  nrow=1, ncol=6)
+colnames(pvalues) <- c("Yu", "Yu Aff", "Yu Coun", "Leung", "Leung Aff", "Leung Coun")
 rownames(pvalues) <- "P-values"
 pvalues
